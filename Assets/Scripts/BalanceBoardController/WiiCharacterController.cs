ba3f19bd7e1702d8fd259fc57c5c2fae4aa0fa99
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-01-19 15:19:36 +0900

    対象のキャラクターを重心をかけた位置に向かせる

---
+++ Assets/Scripts/BalanceBoardController/WiiCharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiiCharacterController : MonoBehaviour
+{
+    [SerializeField] GameObject characterObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Wii.GetRemoteCount(); ++i)
+        {
+            if (Wii.IsActive(i) && Wii.GetExpType(i) == 3)
+            {
+                Vector2 theCenter = Wii.GetCenterOfBalance(i);
+                Vector3 characterPosition = characterObject.transform.position;
+                characterObject.transform.LookAt(new Vector3(characterPosition.x + theCenter.x, characterPosition.y, characterPosition.z + theCenter.y));
+                break;
+            }
+        }
+    }
+}
